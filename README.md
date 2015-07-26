2f872c15187f186bd392e57d0b8d06cf8a22b6f3
christopheragocs <cagocs@backstopsolutions.com>
2015-07-26 14:21:41 -0500

    some skeleton code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 PLAYS SOUNDS ON A DOOR
+
+
+Mac:
+`brew install portaudio`
+
+Nerdmachine:
+`apt-get install portaudio19-dev`
+
+
+Then:
+`go get code.google.com/p/portaudio-go/portaudio`
+
---
+++ sounds.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"code.google.com/p/portaudio-go/portaudio"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	http.HandleFunc("/play/", func(w http.ResponseWriter, req *http.Request) {
+		if req.Method == "POST" {
+			log.Println("Recieved post request.")
+			playASound(req.FormFile("soundFile"))
+			//TODO(cagocs): play a sound
+
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+
+			//TODO(cagocs): maybe return 200 with the name of the sound playing?
+		}
+	})
+
+}
+
+// Muchos gracias to https://gist.github.com/isaiah/5699797
+func playASound(file multipart.File) {
+	chk := func(err error) {
+		if err != nil {
+			panic(err)
+		}
+	}
+	framePerBuffer := 2048
+	ff := newFfmpeg(file)
+	defer ff.Close()
+	stream, err := portaudio.OpenDefaultStream(0, 2, 44100, framePerBuffer, ff)
+	chk(err)
+	defer stream.Close()
+	chk(stream.Start())
+	if err := ff.cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	chk(stream.Stop())
+}
+
+type ffmpeg struct {
+	in  io.ReadCloser
+	cmd *exec.Cmd
+}
+
+func newFfmpeg(filename string) *ffmpeg {
+	cmd := exec.Command("ffmpeg", "-i", filename, "-f", "s16le", "-")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	return &ffmpeg{stdout, cmd}
+}
+
+func (f *ffmpeg) Close() error {
+	return f.in.Close()
+}
+
+func (f *ffmpeg) ProcessAudio(_, out [][]int16) {
+	// int16 takes 2 bytes
+	bufferSize := len(out[0]) * 4
+	var pack = make([]byte, bufferSize)
+	if _, err := f.in.Read(pack); err != nil {
+		log.Fatal(err)
+	}
+	n := make([]int16, len(out[0])*2)
+	for i := range n {
+		var x int16
+		buf := bytes.NewBuffer(pack[2*i : 2*(i+1)])
+		binary.Read(buf, binary.LittleEndian, &x)
+		n[i] = x
+	}
+
+	for i := range out[0] {
+		out[0][i] = n[2*i]
+		out[1][i] = n[2*i+1]
+	}
+}
